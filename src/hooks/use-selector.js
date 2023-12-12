4feaaea33330fb724805450a1c5714993b24d040
Andrey Starostin <andreystarostin.vio@gmail.com>
2023-12-12 21:33:52 +0300

    final merge commit

--- src/hooks/use-selector.js
+++ src/hooks/use-selector.js
@@ -1,20 +1,5 @@
-<<<<<<< HEAD:src/store/use-selector.js
-import useStore from "./use-store";
 import { useEffect, useLayoutEffect, useMemo, useState } from "react";
 import shallowequal from "shallowequal";
-
-/**
- * Хук для выборки данных из store и отслеживания их изменения
- * @param selector {Function}
- * @return {*}
- */
-export default function useSelector(selector) {
-  const store = useStore();
-
-  const [state, setState] = useState(() => selector(store.getState()));
-=======
-import {useEffect, useLayoutEffect, useMemo, useState} from "react";
-import shallowequal from 'shallowequal';
 import useStore from "./use-store";
 
 /**
@@ -26,20 +11,14 @@ export default function useSelector(selectorFunc) {
   const store = useStore();
 
   const [state, setState] = useState(() => selectorFunc(store.getState()));
->>>>>>> 0fe02a795006318a9dd0318710fa5fa608ef246f:src/hooks/use-selector.js
 
   const unsubscribe = useMemo(() => {
     // Подписка. Возврат функции для отписки
     return store.subscribe(() => {
-<<<<<<< HEAD:src/store/use-selector.js
-      const newState = selector(store.getState());
+      const newState = selectorFunc(store.getState());
       setState((prevState) =>
         shallowequal(prevState, newState) ? prevState : newState
       );
-=======
-      const newState = selectorFunc(store.getState());
-      setState(prevState => shallowequal(prevState, newState) ? prevState : newState);
->>>>>>> 0fe02a795006318a9dd0318710fa5fa608ef246f:src/hooks/use-selector.js
     });
   }, []); // Нет зависимостей - исполнится один раз
 
--- src/store/catalog/index.js
+++ src/store/catalog/index.js
@@ -1,65 +1,9 @@
-<<<<<<< HEAD
-import { codeGenerator } from "../../utils";
-import StoreModule from "../module";
-
-class Catalog extends StoreModule {
-  constructor(store, name) {
-    super(store, name);
-    this.generateCode = codeGenerator(0);
-  }
-
-  initState() {
-    return {
-      list: [],
-      loading: false,
-    };
-  }
-
-  async load(skip) {
-    try {
-      this.setState(
-        {
-          ...this.getState(),
-          loading: true,
-        },
-        `Загружаются товары из АПИ`
-      );
-      const response = await fetch(
-        `/api/v1/articles?limit=10&skip=${skip}&fields=items(_id, title, price),count`
-      );
-      const json = await response.json();
-
-      this.setState(
-        {
-          ...this.getState(),
-          list: json.result.items,
-          count: json.result.count,
-        },
-        "Загружены товары из АПИ"
-      );
-    } catch (error) {
-      console.error(error);
-    } finally {
-      this.setState(
-        {
-          ...this.getState(),
-          loading: false,
-        },
-        `Загрузка товаров из АПИ завершена`
-      );
-    }
-  }
-}
-
-export default Catalog;
-=======
 import StoreModule from "../module";
 
 /**
  * Состояние каталога - параметры фильтра и список товара
  */
 class CatalogState extends StoreModule {
-
   /**
    * Начальное состояние
    * @return {Object}
@@ -70,12 +14,12 @@ class CatalogState extends StoreModule {
       params: {
         page: 1,
         limit: 10,
-        sort: 'order',
-        query: ''
+        sort: "order",
+        query: "",
       },
       count: 0,
-      waiting: false
-    }
+      waiting: false,
+    };
   }
 
   /**
@@ -87,11 +31,16 @@ class CatalogState extends StoreModule {
   async initParams(newParams = {}) {
     const urlParams = new URLSearchParams(window.location.search);
     let validParams = {};
-    if (urlParams.has('page')) validParams.page = Number(urlParams.get('page')) || 1;
-    if (urlParams.has('limit')) validParams.limit = Math.min(Number(urlParams.get('limit')) || 10, 50);
-    if (urlParams.has('sort')) validParams.sort = urlParams.get('sort');
-    if (urlParams.has('query')) validParams.query = urlParams.get('query');
-    await this.setParams({...this.initState().params, ...validParams, ...newParams}, true);
+    if (urlParams.has("page"))
+      validParams.page = Number(urlParams.get("page")) || 1;
+    if (urlParams.has("limit"))
+      validParams.limit = Math.min(Number(urlParams.get("limit")) || 10, 50);
+    if (urlParams.has("sort")) validParams.sort = urlParams.get("sort");
+    if (urlParams.has("query")) validParams.query = urlParams.get("query");
+    await this.setParams(
+      { ...this.initState().params, ...validParams, ...newParams },
+      true
+    );
   }
 
   /**
@@ -101,7 +50,7 @@ class CatalogState extends StoreModule {
    */
   async resetParams(newParams = {}) {
     // Итоговые параметры из начальных, из URL и из переданных явно
-    const params = {...this.initState().params, ...newParams};
+    const params = { ...this.initState().params, ...newParams };
     // Установка параметров и загрузка данных
     await this.setParams(params);
   }
@@ -113,42 +62,50 @@ class CatalogState extends StoreModule {
    * @returns {Promise<void>}
    */
   async setParams(newParams = {}, replaceHistory = false) {
-    const params = {...this.getState().params, ...newParams};
+    const params = { ...this.getState().params, ...newParams };
 
     // Установка новых параметров и признака загрузки
-    this.setState({
-      ...this.getState(),
-      params,
-      waiting: true
-    }, 'Установлены параметры каталога');
+    this.setState(
+      {
+        ...this.getState(),
+        params,
+        waiting: true,
+      },
+      "Установлены параметры каталога"
+    );
 
     // Сохранить параметры в адрес страницы
     let urlSearch = new URLSearchParams(params).toString();
-    const url = window.location.pathname + '?' + urlSearch + window.location.hash;
+    const url =
+      window.location.pathname + "?" + urlSearch + window.location.hash;
     if (replaceHistory) {
-      window.history.replaceState({}, '', url);
+      window.history.replaceState({}, "", url);
     } else {
-      window.history.pushState({}, '', url);
+      window.history.pushState({}, "", url);
     }
 
     const apiParams = {
       limit: params.limit,
       skip: (params.page - 1) * params.limit,
-      fields: 'items(*),count',
+      fields: "items(*),count",
       sort: params.sort,
-      'search[query]': params.query
+      "search[query]": params.query,
     };
 
-    const response = await fetch(`/api/v1/articles?${new URLSearchParams(apiParams)}`);
+    const response = await fetch(
+      `/api/v1/articles?${new URLSearchParams(apiParams)}`
+    );
     const json = await response.json();
-    this.setState({
-      ...this.getState(),
-      list: json.result.items,
-      count: json.result.count,
-      waiting: false
-    }, 'Загружен список товаров из АПИ');
+    this.setState(
+      {
+        ...this.getState(),
+        list: json.result.items,
+        count: json.result.count,
+        waiting: false,
+      },
+      "Загружен список товаров из АПИ"
+    );
   }
 }
 
 export default CatalogState;
->>>>>>> 0fe02a795006318a9dd0318710fa5fa608ef246f
